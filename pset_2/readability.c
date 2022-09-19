b672a3379039169051d3fe145757d9142f437686
JB <jacobbunnellca@gmail.com>
2022-09-19 16:02:17 -0400

    counting function omits spaces

--- pset_2/readability.c
+++ pset_2/readability.c
@@ -1,9 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
 
 int main(void)
 {
-    //Getting user input
-    string prompt = get_string("Text: ");
-    printf("%s\n", prompt);
+    // Getting user input
+    string text = get_string("Text: ");
+    int results = count_letters(text);
+    printf("%i letters\n", results);
+}
+
+
+// Counts number of letters in a string
+int count_letters(string text)
+{
+    int count = 0;
+    string s = text;
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        // Omits spaces
+        if(s[i] != ' ')
+        {
+          count++;
+        }
+    }
+    return count;
 }
