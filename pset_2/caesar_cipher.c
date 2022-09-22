0803c96885f888df3db3f8a9cb6c49e9ad5966ec
JB <jacobbunnellca@gmail.com>
2022-09-22 13:19:58 -0400

    Pseudocode for caesar cipher

---
+++ pset_2/caesar_cipher.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // Make sure program is used with command line argument
+
+    // Make sure every character in argument is an digit
+
+    // Convert argument from string to an integer
+
+    // Prompt user for input - plaintext
+
+    // For each character in plaintext
+
+        // Rotate character if it's a letter
+}
