59de6c7cbca6df21c7d0e86e1ecba58afec7f6eb
JB <jacobbunnellca@gmail.com>
2022-09-23 11:24:36 -0400

    function added to ensure character in argument is a digit

--- pset_2/caesar_cipher.c
+++ pset_2/caesar_cipher.c
@@ -1,8 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Prototypes
+bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
+
     // Make sure program contains command line argument
     int n = argc;
     if (n <= 1 || n > 2)
@@ -10,7 +16,6 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
     }
     return 0;
-
 
     // Make sure every character in argument is an digit
 
@@ -21,4 +26,18 @@ int main(int argc, string argv[])
     // For each character in plaintext
 
         // Rotate character if it's a letter
+}
+
+// Make sure every character in argument is an digit
+
+bool only_digits(string s)
+{
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (s[i] >= 10 || s[i] < 0)
+        {
+            return 0;
+        }
+    }
+    return true;
 }
